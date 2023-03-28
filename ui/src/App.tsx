ca345e2e00b5cd9a2090d3da68f2f97eecaec308
lukasVirtual <gerlinskilukas@gmail.com>
2023-03-28 16:10:50 +0200

    add mui + simple table

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,13 +1,20 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import SoftwareList from "./components/Software/software.tsx"
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
-
   return (
-    <div>
-       <h1>hello world</h1>
-    </div>
+  <ThemeProvider theme={darkTheme}>
+  <CssBaseline />
+      <SoftwareList />
+  </ThemeProvider>
+
   );
 }
 
